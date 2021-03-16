89b044b6d39b6e9ff804c6a27ebaef1abd1e47d4
cchura94 <cchura.cpc@gmail.com>
2021-03-15 20:00:09 -0400

    Rutas - Controladores - vistas

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UsuarioController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource("/categoria", CategoriaController::class);
+Route::resource("/producto", ProductoController::class);
+Route::resource("/cliente", ClienteController::class);
+Route::resource("/pedido", PedidoController::class);
+Route::resource("/usuario", UsuarioController::class);
+Route::resource("/role", RoleController::class);
+
